57086aa9d2f9a1c766171f97e6472350163c3c19
jcg0105 <juliagriner00@gmail.com>
2019-10-20 21:23:01 -0400

    no message

---
+++ Transportation Project/Assets/Scripts/CubeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    { //if clicked, turn cube red. otherwise turn white
+        if (GameController.activeCube != null)
+        {
+            GameController.activeCube.GetComponent<Renderer>().material.color = Color.white;
+        }
+
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+        GameController.activeCube = gameObject;
+    }
+}
---
+++ Transportation Project/Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject cubePreFab;
+
+    Vector3 cubePosition;
+
+    public static GameObject activeCube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i<16; i++)
+        {
+            cubePosition = new Vector3(i*2, 0, 0);
+            Instantiate(cubePreFab, cubePosition, Quaternion.identity);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
